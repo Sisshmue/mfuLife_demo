39f2bf0656344e500af566eb971d6cd5c01ecb82
Siss-Hmue <6531503184@lamduan.mfu.ac.th>
2024-09-11 01:30:58 +0700

    push notification demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mfu_life_demo/api/firebase_api.dart';
 import 'package:mfu_life_demo/firebase_options.dart';
 import 'package:mfu_life_demo/provider/eventButtonProvider.dart';
+import 'package:mfu_life_demo/screen/eventsHome.dart';
 import 'package:mfu_life_demo/screen/homePage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotification();
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
@@ -25,8 +28,12 @@ class Demo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage(),
+      navigatorKey: navigatorKey,
+      routes: {
+        '/EventsScreen': (context) => Events(),
+      },
     );
   }
 }
--- lib/screen/homePage.dart
+++ lib/screen/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'eventsHome.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home",
+          style: GoogleFonts.openSans(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        backgroundColor: const Color(0xFF7A060D),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
